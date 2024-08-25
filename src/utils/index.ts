fa9ef7384bf4836638d0be6da7c0635c007f8de7
Justin Fernald <jfernald@my.gcu.edu>
2024-08-25 01:14:04 -0700

    set colors on cube

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export const cubeColors = {
+  white: '#FFFFFF',
+  yellow: '#ffe710',
+  blue: '#00ccff',
+  green: '#00ff15',
+  red: '#ff3a3a',
+  orange: '#e77b00',
+  internals: '#000000',
+} as const;
